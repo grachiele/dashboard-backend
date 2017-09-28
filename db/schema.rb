e537f702a56bc1cbc3202786136590a1ef4d0c04
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-09-28 14:36:17 -0400

    added zipcode to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928140713) do
+ActiveRecord::Schema.define(version: 20170928180117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170928140713) do
     t.boolean "weather"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zipcode"
   end
 
 end
