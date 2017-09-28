ebefe2b389b8904e54063cb066705fa25e84ed58
Neil Steiner <npsteiner@gmail.com>
2017-09-28 15:23:07 -0400

    just added comments to controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,30 +1,36 @@
 class Api::V1::UsersController < ApplicationController
 
+# don't think these will be used
   def new
   end
+  def edit
+  end
+#---------------------------------------------------------
 
+# signup
   def create
     @user = User.create(user_params)
   end
 
-  def edit
-  end
-
+# edit preferences
   def update
   end
 
+# delete account
   def destroy
   end
 
+# this will be the user's homepage ?
   def show
     @user = User.find(params[:id])
     render json: @user
   end
 
+
   private
 
   def user_params
-    params.permit(:username, :password, :news, :weather)
+    params.permit(:username, :password, :news, :weather, :zipcode)
   end
 
 end
