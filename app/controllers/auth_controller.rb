41e72bb3eabd80f7088f12bd0ff726dab946a530
Neil Steiner <npsteiner@gmail.com>
2017-09-30 13:09:51 -0400

    modified auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,7 +5,6 @@ class AuthController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      #YOU ARE WHO YOU SAY YOU ARE
       token = encode_token({ user_id: user.id})
       render json: { user: user, jwt: token}
     end
