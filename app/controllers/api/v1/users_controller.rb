2a7a5fc1653c2d534c1585cc8f407bd7b2cff2b8
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-10-02 17:12:57 -0400

    user creation done.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,7 +9,11 @@ class Api::V1::UsersController < ApplicationController
 
 # signup
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      token = encode_token({ user_id: @user.id})
+      render json: { user: @user, jwt: token}
+    end
   end
 
 # edit preferences
